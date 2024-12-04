0e0494a5ed6cbc34820e42486b0318441dcee140
iamelmirr <besirovicelmir36@gmail.com>
2024-12-04 22:52:25 +0100

    v1

---
+++ pokedex/src/components/SideNav.jsx
@@ -0,0 +1,20 @@
+import { first151Pokemon, getFullPokedexNumber } from "../utils"
+
+export default function SideNav() {
+    return (
+        <nav>
+            <div className={'header'}>
+                <h1 className="text-gradient">Pokedex</h1>
+            </div>
+            <input  />
+            {first151Pokemon.map((pokemon, pokemonIndex) => {
+                return (
+                    <button key={pokemonIndex} className={'nav-card '}>
+                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        <p>{pokemon}</p>
+                    </button>
+                )
+            })}
+        </nav>
+    )
+}
