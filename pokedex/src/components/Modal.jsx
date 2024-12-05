f599fd1c426595ab85a9cf8adee46819d2f30f1a
iamelmirr <besirovicelmir36@gmail.com>
2024-12-05 16:39:38 +0100

    v2

--- pokedex/src/components/Modal.jsx
+++ pokedex/src/components/Modal.jsx
@@ -4,10 +4,11 @@ export default function Modal(props) {
     const { children, handleCloseModal } = props
     return ReactDom.createPortal(
         <div className='modal-container'>
-            <button onClick={handleCloseModal} className='modal-underlay'/>
+            <button onClick={handleCloseModal} className='modal-underlay' />
             <div className='modal-content'>
                 {children}
             </div>
-        </div>, document.getElementById('portal')
+        </div>,
+        document.getElementById('portal')
     )
 }
--- pokedex/src/components/TypeCard.jsx
+++ pokedex/src/components/TypeCard.jsx
@@ -2,8 +2,8 @@ import { pokemonTypeColors } from "../utils"
 
 export default function TypeCard(props) {
     const { type } = props
-    return(
-        <div className="type-tile" style={{color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background}}>
+    return (
+        <div className="type-tile" style={{ color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background }}>
             <p>{type}</p>
         </div>
     )
